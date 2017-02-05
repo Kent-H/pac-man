2d7cbadd1ec1e3f7645a647c5e592a9198aa12ae
Kent Hagerman <builderk.doomsday@gmail.com>
2017-02-05 18:21:10 -0500

    Added maze generation algorithm.  Also added a second AI (Blinky) which tries to move towards the player.

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -8,12 +8,25 @@ public class Ghost : MonoBehaviour
     //distance travelled per game tick
     public float Speed = 0.017f;
 
+    //AI to use, 
+    public string AI = "Clyde";
+
     //direction that this ghost is moving
     Vector2 direction = Vector2.up;
 
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
     void FixedUpdate()
     {
-        RandomizeDirection();
+        if (AI.ToLowerInvariant() == "blinky")
+            ChasePlayer();
+        else
+            RandomizeDirection();
 
         //if there is open space
         if (isValidMove(direction, Speed))
@@ -50,6 +63,38 @@ public class Ghost : MonoBehaviour
 
         //if we've reached a dead end, go back the way we came
         direction = -direction;
+    }
+
+    //determine the next step based on which direction will move closer to the player
+    void ChasePlayer()
+    {
+        //build a list of options, every direction except a 180 turn is included
+        var options = new List<Vector2>();
+        options.Add(direction);
+        options.Add(new Vector2(-direction.y, direction.x));
+        options.Add(new Vector2(direction.y, -direction.x));
+
+        //determine the direction that is most towards the player (original pac-man behavior for blinky)
+        Vector2 closestDirection = Vector2.zero;
+        float closestDistance = Mathf.Infinity;
+
+        for (int i = 0; i < options.Count; i++)
+        {
+            float playerDistance = Vector2.Distance((Vector2)transform.position + options[i] * Speed, player.transform.position);
+            if (playerDistance < closestDistance && isValidMove(options[i], Speed))
+            {
+                //valid direction found, we will go this way
+                closestDirection = options[i];
+                closestDistance = playerDistance;
+                Debug.Log(playerDistance);
+            }
+        }
+
+        if (closestDirection != Vector2.zero)
+            direction = closestDirection;
+        else
+            //if we've reached a dead end, go back the way we came
+            direction = -direction;
     }
 
     //helper to check if moving would cause a collision
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,37 +4,248 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    //size of the game grid
+    public int Size = 11;
 
-    const int SIZE = 10;
+    public float TileSize = 0.17f;
 
     // Use this for initialization
     void Start()
     {
+        //store grid space that has already been used
+        bool[,] complete = new bool[11, 11];
 
+        //horizontal and vertical walls
+        bool[,] vWalls = new bool[Size - 1, Size];
+        bool[,] hWalls = new bool[Size, Size - 1];
+
+        //block off the middle where ghosts spawn(2x1 box)
+        vWalls[Size / 2 + 1, Size / 2] = true;
+        vWalls[Size / 2 - 1, Size / 2] = true;
+        hWalls[Size / 2, Size / 2 - 1] = true;
+        hWalls[Size / 2 + 1, Size / 2] = true;
+        hWalls[Size / 2 + 1, Size / 2 - 1] = true;
+
+        //plus one hallway around it. (4x3 box)
+        for (int x = 0; x < 4; x++)
+        {
+            //for the purpose of finding hallways, mark the entire box as used
+            for (int y = 0; y < 3; y++)
+                complete[Size / 2 + x - 1, Size / 2 + y - 1] = true;
+
+            hWalls[Size / 2 + x - 1, Size / 2 + 1] = true;
+            hWalls[Size / 2 + x - 1, Size / 2 - 2] = true;
+        }
+
+        for (int y = 0; y < 3; y++)
+        {
+            vWalls[Size / 2 - 2, Size / 2 + y - 1] = true;
+            vWalls[Size / 2 + 2, Size / 2 + y - 1] = true;
+        }
+
+        for (int i = 0; i < 10; i++)
+            RandomHallway(complete, vWalls, hWalls, 0.4f);
+
+        for (int x = 0; x < Size; x++)
+            for (int y = 0; y < Size; y++)
+                if (complete[x, y])
+                    Debug.DrawLine(
+                        new Vector3((x - Size / 2f + 0.5f) * 0.17f, (y - Size / 2f + 0.5f) * 0.17f - 0.04f, 0),
+                        new Vector3((x - Size / 2f + 0.5f) * 0.17f, (y - Size / 2f + 0.5f) * 0.17f, 0), Color.green, 100);
+
+        Generate(complete, vWalls, hWalls);
+    }
+
+
+    //places entities into the game
+    void Generate(bool[,] complete, bool[,] vertical, bool[,] horizontal)
+    {
         var dot = Resources.Load("Prefabs/Dot");
         var wall = Resources.Load("Prefabs/Wall");
 
-        bool[,] complete = new bool[10, 10];
-
-        //generate the middle
-        for (int x = -2; x <= 1; x++)
+        //walls around the map
+        for (int i = 0; i < Size; i++)
         {
-            for (int y = -2; y <= 1; y++)
+            Instantiate(wall, new Vector2((-Size / 2f + i + 0.5f) * TileSize, (Size / 2f) * TileSize), Quaternion.Euler(0, 0, 90));
+            Instantiate(wall, new Vector2((-Size / 2f + i + 0.5f) * TileSize, (-Size / 2f) * TileSize), Quaternion.Euler(0, 0, 90));
+            Instantiate(wall, new Vector2((Size / 2f) * TileSize, (-Size / 2f + i + 0.5f) * TileSize), Quaternion.identity);
+            Instantiate(wall, new Vector2((-Size / 2f) * TileSize, (-Size / 2f + i + 0.5f) * TileSize), Quaternion.identity);
+        }
+
+        //dot at the center of every grid tile
+        for (int x = 0; x < Size; x++)
+            for (int y = 0; y < Size; y++)
+                //only generate walls inside the grid
+                if (complete[x, y])
+                    Instantiate(dot, new Vector2((-Size / 2f + x + 0.5f) * TileSize, (-Size / 2f + y + 0.5f) * TileSize), Quaternion.identity);
+
+        //all vertical walls, or dots where there are no walls
+        for (int x = 0; x < vertical.GetLength(0); x++)
+            for (int y = 0; y < vertical.GetLength(1); y++)
             {
-                Instantiate(dot, new Vector2(x * 0.17f, y * 0.17f), Quaternion.identity);
+                if (vertical[x, y])
+                {
+                    Instantiate(wall, new Vector2((-Size / 2f + x + 1) * TileSize, (-Size / 2f + y + 0.5f) * TileSize), Quaternion.identity);
+                }
+                else
+                {
+                    if (complete[x, y] || complete[x + 1, y])
+                        Instantiate(dot, new Vector2((-Size / 2f + x + 1) * TileSize, (-Size / 2f + y + 0.5f) * TileSize), Quaternion.identity);
+                }
+            }
+
+        //all horizontal walls, dots where there are no walls
+        for (int x = 0; x < horizontal.GetLength(0); x++)
+            for (int y = 0; y < horizontal.GetLength(1); y++)
+            {
+                if (horizontal[x, y])
+                {
+                    Instantiate(wall, new Vector2((-Size / 2f + x + 0.5f) * TileSize, (-Size / 2f + y + 1) * TileSize), Quaternion.Euler(0, 0, 90));
+                }
+                else
+                {
+                    if (complete[x, y] || complete[x, y + 1])
+                        Instantiate(dot, new Vector2((-Size / 2f + x + +0.5f) * TileSize, (-Size / 2f + y + 1) * TileSize), Quaternion.identity);
+                }
+            }
+    }
+
+    void RandomHallway(bool[,] complete, bool[,] vWalls, bool[,] hWalls, float endChance)
+    {
+        Vector2 position, direction;
+        FindRandomStart(complete, out position, out direction);
+
+        Debug.DrawLine(Vector3.zero, direction, Color.green, 1000);
+
+        int hallwayLength = 0;
+        while (true)
+        {
+            hallwayLength++;
+            //mark this tile complete
+            complete[(int)position.x, (int)position.y] = true;
+
+            //create a wall on every side of this tile
+            //except the direction we came from, remove that wall instead
+            //up
+            if (IsValidHWall(position.x, position.y))
+                hWalls[(int)position.x, (int)position.y] = direction != Vector2.down;
+            //down
+            if (IsValidHWall(position.x, position.y - 1))
+                hWalls[(int)position.x, (int)position.y - 1] = direction != Vector2.up;
+            //left
+            if (IsValidVWall(position.x - 1, position.y))
+                vWalls[(int)position.x - 1, (int)position.y] = direction != Vector2.right;
+            //right
+            if (IsValidVWall(position.x, position.y))
+                vWalls[(int)position.x, (int)position.y] = direction != Vector2.left;
+
+
+            Vector2 left = new Vector2(-direction.y, direction.x);
+            Vector2 right = new Vector2(direction.y, -direction.x);
+
+            //should we end the hallway?
+            if (hallwayLength > 3 && Random.value < endChance &&
+                IsInBounds(position.x + left.x, position.y + left.y) &&
+                complete[(int)(position.x + left.x), (int)(position.y + left.y)])
+            {
+                //to end the hallway, attach it to the left
+                if (left == Vector2.up)
+                    hWalls[(int)position.x, (int)position.y] = false;
+                else if (left == Vector2.down)
+                    hWalls[(int)position.x, (int)position.y - 1] = false;
+                else if (left == Vector2.left)
+                    vWalls[(int)position.x - 1, (int)position.y] = false;
+                else if (left == Vector2.right)
+                    vWalls[(int)position.x, (int)position.y] = false;
+
+                return;
+            }
+
+            //determine which direction to go next
+
+            //always try to go left first
+            if (IsInBounds(position.x + left.x, position.y + left.y) &&
+                !complete[(int)(position.x + left.x), (int)(position.y + left.y)])
+            {
+                direction = left;
+            }
+            // then try straight
+            else if (IsInBounds(position.x + direction.x, position.y + direction.y) &&
+                !complete[(int)(position.x + direction.x), (int)(position.y + direction.y)])
+            {
+                //nothing to do
+            }
+            //finally try right
+            else if (IsInBounds(position.x + right.x, position.y + right.y) &&
+                !complete[(int)(position.x + right.x), (int)(position.y + right.y)])
+            {
+                direction = right;
+            }
+            //if we're trapped in a corner
+            else
+            {
+                return;
+            }
+
+            position += direction;
+        }
+
+    }
+
+
+    void FindRandomStart(bool[,] complete, out Vector2 position, out Vector2 direction)
+    {
+        //start in the center and pick a random direction
+        float dir = Random.value * 2 * Mathf.PI;
+        float xAmount = Mathf.Cos(dir);
+        float yAmount = Mathf.Sin(dir);
+        Debug.DrawLine(Vector3.zero, new Vector3(xAmount, yAmount, 0), Color.green, 1000);
+        direction = Vector2.zero;
+        int x = 0, y = 0;
+
+        //march until an unused space is found
+        while (!IsInBounds(x + Size / 2, y + Size / 2) || complete[x + Size / 2, y + Size / 2])
+        {
+            if ((Mathf.Abs(x) + 1) / Mathf.Abs(xAmount) < (Mathf.Abs(y) + 1) / Mathf.Abs(yAmount))
+            {
+                direction = xAmount > 0 ? Vector2.right : Vector2.left;
+                x += xAmount > 0 ? 1 : -1;
+            }
+            else
+            {
+                direction = yAmount > 0 ? Vector2.up : Vector2.down;
+                y += yAmount > 0 ? 1 : -1;
+            }
+
+            //if we leave the grid without finding an open space
+            if (x + Size / 2 < 0 || y + Size / 2 < 0 || x + Size / 2 > Size || y + Size / 2 > Size)
+            {
+                //restart
+                dir = Random.value * 2 * Mathf.PI;
+                xAmount = Mathf.Cos(dir);
+                yAmount = Mathf.Sin(dir);
+                x = y = 0;
             }
         }
 
-        for (int i = -SIZE/2; i <= SIZE/2; i++)
-        {
-            Instantiate(wall, new Vector2(i * 0.17f, SIZE * 0.17f / 2), Quaternion.Euler(0, 0, 90));
-            Instantiate(wall, new Vector2(i * 0.17f, -SIZE * 0.17f / 2), Quaternion.Euler(0, 0, 270));
-            Instantiate(wall, new Vector2(SIZE * 0.17f / 2, i * 0.17f), Quaternion.identity);
-            Instantiate(wall, new Vector2(-SIZE * 0.17f / 2, i * 0.17f), Quaternion.Euler(0, 0, 180));
-        }
+        position = new Vector2(x + Size / 2, y + Size / 2);
+    }
 
-        //Instantiate(dot, new Vector2(x * 0.15f, y * 0.15f), Quaternion.identity);
+    bool IsInBounds(float x, float y)
+    {
+        return x >= 0 && y >= 0 && x < Size && y < Size;
+    }
 
+    bool IsValidVWall(float x, float y)
+    {
+        //vertical walls have size-1 on the x-axis
+        return x >= 0 && y >= 0 && x < Size - 1 && y < Size;
+    }
+
+    bool IsValidHWall(float x, float y)
+    {
+        //horizontal walls have size-1 on the y-axis
+        return x >= 0 && y >= 0 && x < Size && y < Size - 1;
     }
 
 }
